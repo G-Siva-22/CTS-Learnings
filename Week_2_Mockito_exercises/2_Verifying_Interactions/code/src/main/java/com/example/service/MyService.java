9c3f256bd115d115a0efe171839b8606eb5700c7
G-Siva-22 <sivamsecit22@gmail.com>
2025-06-29 19:41:07 +0530

    uploaded

---
+++ Week_2_Mockito_exercises/2_Verifying_Interactions/code/src/main/java/com/example/service/MyService.java
@@ -0,0 +1,15 @@
+package com.example.service;
+
+import com.example.api.ExternalApi;
+
+public class MyService {
+    private final ExternalApi externalApi;
+
+    public MyService(ExternalApi externalApi) {
+        this.externalApi = externalApi;
+    }
+
+    public String fetchData() {
+        return externalApi.getData();
+    }
+}

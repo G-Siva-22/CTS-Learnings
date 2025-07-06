8f3fdecdde24b3b4fd2a292a41b70e394aaeac36
G-Siva-22 <sivamsecit22@gmail.com>
2025-07-06 22:46:42 +0530

    uploaded

---
+++ Week_3_Spring_Core_and_Maven/Spring_Core_Maven/2_Implementing_Dependency_Injection/code/src/main/java/com/example/library/LibraryManagementApplication.java
@@ -0,0 +1,16 @@
+package com.example.library;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService bookService = context.getBean("bookService", BookService.class);
+        bookService.performService();
+
+        System.out.println("Dependency Injection via XML configuration works!");
+    }
+}
